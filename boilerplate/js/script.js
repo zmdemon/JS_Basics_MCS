75cc8cfd88ce9b9271cea6bcce7b0b09fc5968da
zmdemon <dmitriy.gaev66@gmail.com>
2021-02-03 22:31:37 +0300

    Update script.js

--- boilerplate/js/script.js
+++
@@ -1,8 +0,0 @@
-
-let totalCash = Number(prompt("how much cash is in your bag?"));
-let watchesCount =  Number(prompt("how many watches?"));
-let earringsCount = Number(prompt("how many earrings?"));
-let watchesSum = Number(prompt("one watch cost?"));
-let earringsSum = Number(prompt("one earrings pair cost?"));
-
-document.body.innerHTML = watchesCount*watchesSum + earringsCount*earringsSum <= totalCash;
