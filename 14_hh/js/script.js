9b7fe5b829af573a2c610282a6a0069a8ca1825e
zmdemon <dmitriy.gaev66@gmail.com>
2021-02-07 19:19:43 +0300

    practice 5.11.1

---
+++ 14_hh/js/script.js
@@ -0,0 +1,28 @@
+let newSchool = [
+    [
+        ['Lil Yachty', 'Playboi Carti', 'Lil Uzi Vert'],
+        ['Sonny Digital', 'Metro Boomin', '12Hunna']
+    ],
+    [
+        ['Yung Lean', 'Ecco2K', 'Bladee'],
+        ['Whitearmor', 'PJ Beats', 'Yung Gud']
+    ]
+];
+
+newSchool.forEach(function (country){
+    country.forEach(function (profession) {
+        profession.forEach(function (name){
+            console.log(name);
+        })
+    })
+})
+
+for (country in newSchool) {
+    for (profession in newSchool[country]) {
+        for (name in newSchool[country][profession])
+        console.log(newSchool[country][profession][name]);
+    }
+}
+
+
+
