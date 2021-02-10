57a5afa1894a81ca7bb63957fcf1a79405603a23
zmdemon <dmitriy.gaev66@gmail.com>
2021-02-10 20:48:49 +0300

    reworked

--- 08_kg-fun/js/script.js
+++ 08_kg-fun/js/script.js
@@ -1,24 +1,10 @@
-
-
-let totalCash = Number(prompt("how much cash is in your bag?"));
-let watchesCount = Number(prompt("how many watches?"));
-let earringsCount = Number(prompt("how many earrings?"));
-let watchesSum = Number(prompt("one watch cost?"));
-let earringsSum = Number(prompt("one earrings pair cost?"));
-//const result = document.selectorQuery("#result");
-
-// 1 вариант
-let haveEnough = (totalCash, watchesCount, earringsCount, watchesSum,earringsSum) => {	
-	if ((watchesCount*watchesSum + earringsCount*earringsSum) <= totalCash) {
-		return "Enough $$$";
-	} else return "Not enough $$$";
+function haveEnough() {
+    const totalCash = Number(prompt("how much cash is in your bag?"));
+    const watchesCount = Number(prompt("how many watches?"));
+    const earringsCount = Number(prompt("how many earrings?"));
+    const watchesSum = Number(prompt("one watch cost?"));
+    const earringsSum = Number(prompt("one earrings pair cost?"));
+    if ((watchesCount * watchesSum + earringsCount * earringsSum) <= totalCash) {
+        return "Enough $$$";
+    } else return "Not enough $$$";
 }
-
-// 2 вариант (верный)
-function haveEnough(totalCash, watchesCount, earringsCount, watchesSum, earringsSum) {	
-	if ((watchesCount*watchesSum + earringsCount*earringsSum) <= totalCash) {
-		return "Enough $$$";
-	} else return "Not enough $$$";
-}
-
-//result.innerHTML = haveEnough(totalCash, watchesCount, earringsCount, watchesSum,earringsSum);
